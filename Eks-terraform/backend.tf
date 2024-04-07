9171f3a21c3d30d85f35169146d5a0728370796f
Darshil Patel <darshilpatel1999@gmail.com>
2024-04-07 18:00:19 -0400

    changes to backend file

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "patedarmanulife" # Replace with your actual S3 bucket name
+    bucket = "demo-app-bucket-artifact" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
